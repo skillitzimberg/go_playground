543a4b73b88fb18aecb7fe45f38c8dd44addee70
Scott Bergler <commandinghands@gmail.com>
2020-04-30 07:59:01 -0700

    Rename an error handler.

--- unmarshalJson.go
+++ unmarshalJson.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 // How to make this generic, meaning how can I make this work with any targets of any type?
-func unmarshalJSONThingy(s string, target *[]thingy) {
+func unmarshalJSON(s string, target *[]interface{}) interface{} {
 	err := json.Unmarshal([]byte(s), target)
-	printError(err)
-	fmt.Println(target)
+	fakeHandleError(err)
+	return target
 }
