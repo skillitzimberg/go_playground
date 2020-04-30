bd171ead58d369d972322b25998cae90e44a3a6d
Scott Bergler <commandinghands@gmail.com>
2020-04-30 08:01:58 -0700

    Use JSON encoder example.

--- main.go
+++ main.go
@@ -1,20 +1,6 @@
 package main
 
-import "fmt"
-
-type thingy struct {
-	First   string
-	Last    string
-	Age     int
-	Sayings []string
-}
-
 func main() {
-	s := `[{"First":"James","Last":"Bond","Age":32,"Sayings":["Shaken, not stirred","Youth is no guarantee of innovation","In his majesty's royal service"]},{"First":"Miss","Last":"Moneypenny","Age":27,"Sayings":["James, it is soo good to see you","Would you like me to take care of that for you, James?","I would really prefer to be a secret agent myself."]},{"First":"M","Last":"Hmmmm","Age":54,"Sayings":["Oh, James. You didn't.","Dear God, what has James done now?","Can someone please tell me where James Bond is?"]}]`
-
-	var thingies []thingy
-
-	unmarshalJSONThingy(s, &thingies)
-	fmt.Println(thingies)
+	encodeJSON()
 
 }
