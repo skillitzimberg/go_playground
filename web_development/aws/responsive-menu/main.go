786ae5a05560e39f0c57f18ede8ec0dfae84ef8d
Scott Bergler <commandinghands@gmail.com>
2020-06-13 08:37:08 -0700

    Add aws deployment.

---
+++ web_development/aws/responsive-menu/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("views/*.html"))
+}
+
+func serveCSS() {
+	fs := http.FileServer(http.Dir("css"))
+	http.Handle("/css/", http.StripPrefix("/css", fs))
+}
+
+func main() {
+	serveCSS()
+	http.HandleFunc("/", index)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/contact", contact)
+	http.HandleFunc("/projects", projects)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func index(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func about(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "about.html", nil)
+}
+
+func contact(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "contact.html", nil)
+}
+
+func projects(w http.ResponseWriter, req *http.Request) {
+	tpl.ExecuteTemplate(w, "projects.html", nil)
+}
